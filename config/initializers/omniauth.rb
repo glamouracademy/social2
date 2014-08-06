2480bfe3a7f751aab142e21eca8d303a123f9191
Charlotte Chang <yellowspiderlove@gmail.com>
2014-08-06 12:12:47 -0400

    add provider ignores state

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '589073617880756', 'eb201681db543f2539b5951defb7c48e'
+  provider :facebook, '589073617880756', 'eb201681db543f2539b5951defb7c48e', {:provider_ignores_state => true}
 end
